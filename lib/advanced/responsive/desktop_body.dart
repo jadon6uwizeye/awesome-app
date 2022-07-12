a40ab608985ff5a39711f305e5b7471edcafff65
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2022-07-12 17:42:45 +0200

    add mobile view and Desktop View

--- lib/advanced/responsive/desktop_body.dart
+++ lib/advanced/responsive/desktop_body.dart
@@ -5,8 +5,42 @@ class DeskTopBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
-    );
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text("Mobile"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(children: [
+                  AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(
+                      color: const Color.fromARGB(255, 2, 65, 52),
+                    ),
+                  ),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: 8,
+                          itemBuilder: (context, index) {
+                            return const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: ListTile(
+                                tileColor: Color.fromARGB(255, 4, 68, 81),
+                              ),
+                            );
+                          }))
+                ]),
+              ),
+              Container(
+                width: 200,
+                color: const Color.fromARGB(255, 4, 68, 81),
+              )
+            ],
+          ),
+        ));
   }
 }
